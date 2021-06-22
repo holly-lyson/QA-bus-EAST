aa54a0e9d589954e6bb989fcb9835a53f0e24ae8
petroortep <78611123+petroortep@users.noreply.github.com>
2021-06-22 18:44:20 -0400

    contact page frontend done + footer not done yet

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './header'
+import Footer from './footer'
 
 class Home extends Component {
     render() {
       return(
           <div className="Home">
               <Header />
+              {/*<Footer />*/}
           </div>
       );
     }
