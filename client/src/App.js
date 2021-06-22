4620f9b365f3d9c6c3ce96ac6d0828ad28e98f59
Holly Ann Zurenda <zurend55@students.rowan.edu>
2021-06-22 18:36:09 -0400

    Minor changes to header before integrating other branch

--- client/src/App.js
+++ client/src/App.js
@@ -11,18 +11,60 @@ import About from './components/about';
 import Referrals from './components/referrals';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/header';
-
+import Roof from './roof.png'
 
 class App extends Component{
+
+
+
+  toRender(){
+    if (document.location.pathname === "/"){
+    return(
+        <div className="Home">
+        <Header />
+        {/* <MenuBar/> */}
+        <img style={{border: '2px solid black', marginBottom: "5pt", marginTop: "20pt"}}src={Roof} className="Roof" alt="Roof" width="700px" height="auto" />
+        <h1>Services we offer</h1>
+        <h6> Repairs and Full Replacements!</h6>
+          <p>&bull; Shingle Roofing </p>
+          <p>&bull; Flat Roofing </p>
+          <p>&bull; Skylights </p>
+          <p>&bull; Gutters </p>
+          <p>&bull; Tile Roofing </p>
+        <h3> Financing Available! </h3>
+        </div>
+      );
+    } else {
+      return(          
+      <div className="Home">
+        {/* <MenuBar /> */}
+        {/* <Header/> */}
+        <img style={{border: '2px solid black', marginBottom: "5pt", marginTop: "20pt"}}src={Roof} className="Roof" alt="Roof" width="700px" height="auto" />
+        <h1>Services we offer</h1>
+        <h6> Repairs and Full Replacements!</h6>
+          <p>&bull; Shingle Roofing </p>
+          <p>&bull; Flat Roofing </p>
+          <p>&bull; Skylights </p>
+          <p>&bull; Gutters </p>
+          <p>&bull; Tile Roofing </p>
+        <h3> Financing Available! </h3>
+        </div>
+      );
+    }
+  }
   render(){
 
     return(
       <div className="App">
-        <div>
-          <MenuBar/>
+        <div className="Home" style={{marginTop: "0pt"}}>
+          {/* {this.toRender()} */}
+          
         </div>
+        {/* <div> */}
+          <MenuBar/>
+        {/* </div> */}
         <div>
-
+      <div>
       <Router>
          
           <Switch>
@@ -34,13 +76,14 @@ class App extends Component{
             <Route path="/contact"  component={Contact} />
           </Switch>
       </Router>
-
+      </div>
       </div>
 
     </div>
 
     );
   }
+
 }
 
 
