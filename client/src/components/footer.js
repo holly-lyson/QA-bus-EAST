85f2a702b4615bc0f77ede0537b4bbb78c0d7581
petroortep <78611123+petroortep@users.noreply.github.com>
2021-06-23 00:59:01 -0400

    Footer added

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,77 +1,66 @@
-import React from "react";
-import {
-Box,
-Container,
-Row,
-Column,
-FooterLink,
-Heading,
-} from "./footerstyles";
+import React, { Component, useState }  from 'react'
+import './footer.css'
 
-const Footer = () => {
-return (
-	<Box>
-	<h1 style={{ color: "green",
-				textAlign: "center",
-				marginTop: "-50px" }}>
-		GeeksforGeeks: A Computer Science Portal for Geeks
-	</h1>
-	<Container>
-		<Row>
-		<Column>
-			<Heading>About Us</Heading>
-			<FooterLink href="#">Aim</FooterLink>
-			<FooterLink href="#">Vision</FooterLink>
-			<FooterLink href="#">Testimonials</FooterLink>
-		</Column>
-		<Column>
-			<Heading>Services</Heading>
-			<FooterLink href="#">Writing</FooterLink>
-			<FooterLink href="#">Internships</FooterLink>
-			<FooterLink href="#">Coding</FooterLink>
-			<FooterLink href="#">Teaching</FooterLink>
-		</Column>
-		<Column>
-			<Heading>Contact Us</Heading>
-			<FooterLink href="#">Uttar Pradesh</FooterLink>
-			<FooterLink href="#">Ahemdabad</FooterLink>
-			<FooterLink href="#">Indore</FooterLink>
-			<FooterLink href="#">Mumbai</FooterLink>
-		</Column>
-		<Column>
-			<Heading>Social Media</Heading>
-			<FooterLink href="#">
-			<i className="fab fa-facebook-f">
-				<span style={{ marginLeft: "10px" }}>
-				Facebook
-				</span>
-			</i>
-			</FooterLink>
-			<FooterLink href="#">
-			<i className="fab fa-instagram">
-				<span style={{ marginLeft: "10px" }}>
-				Instagram
-				</span>
-			</i>
-			</FooterLink>
-			<FooterLink href="#">
-			<i className="fab fa-twitter">
-				<span style={{ marginLeft: "10px" }}>
-				Twitter
-				</span>
-			</i>
-			</FooterLink>
-			<FooterLink href="#">
-			<i className="fab fa-youtube">
-				<span style={{ marginLeft: "10px" }}>
-				Youtube
-				</span>
-			</i>
-			</FooterLink>
-		</Column>
-		</Row>
-	</Container>
-	</Box>
-);
-};
+class Footer extends Component {
+	render() {
+		return(
+			<body>
+				<div class="container"></div>
+				<footer>
+				{/* <!-- Footer main --> */}
+				<section class="ft-main">
+					<div class="ft-main-item">
+					<h2 class="ft-title"><a href="/about">About</a></h2>
+					<ul class="ft-about-col">
+						<li><a href="/faq">FAQ</a></li>
+						<li><a href="/testimonials">Testimonials</a></li>
+						<li><a href="/referrals">Referrals</a></li>
+					</ul>
+					</div>
+					<div class="ft-main-item">
+					<h2 class="ft-title"><a>Service Area</a></h2>
+					<ul>
+						<li><a>Central Jersey</a></li>
+						<li><a>South Jersey</a></li>
+						<li><a>Philadelphia</a></li>
+					</ul>
+					</div>
+					<div class="ft-main-item">
+					<h2 class="ft-title"><a href="/contact">Contact</a></h2>
+					<ul>
+						<li><a>Jeremy Lyson</a></li>
+						<li><a>609-742-1457</a></li>
+						<li><a>JeremyLyson@email</a></li>
+					</ul>
+					</div>
+					
+				</section>
+				
+				{/* <!-- Footer social --> */}
+				<section class="ft-social">
+					<ul class="ft-social-list">
+					<li><a href="#"><i class="fab fa-facebook"></i></a></li>
+					<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+					<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+					<li><a href="#"><i class="fab fa-github"></i></a></li>
+					<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+					<li><a href="#"><i class="fab fa-youtube"></i></a></li>
+					</ul>
+				</section>
+				
+				{/* <!-- Footer legal --> */}
+				<section class="ft-legal">
+					<ul class="ft-legal-list">
+					<li><a href="#">Terms &amp; Conditions</a></li>
+					<li><a href="#">Privacy Policy</a></li>
+					<li>&copy; 2019 Copyright Nowrap Inc.</li>
+					</ul>
+				</section>
+				</footer>
+			</body>
+
+		)
+	}
+}
+
 export default Footer;
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -7,7 +7,7 @@ class Home extends Component {
       return(
           <div className="Home">
               <Header />
-              {/*<Footer />*/}
+              <Footer />
           </div>
       );
     }
