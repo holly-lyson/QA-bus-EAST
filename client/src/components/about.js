68945283afda56b1cffcd512c62250d6d0aee982
Holly Ann Zurenda <zurend55@students.rowan.edu>
2021-06-23 20:35:25 -0400

    minor background fixes, broke contact page

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -23,7 +23,7 @@ class About extends Component {
                 <br></br>
                 <br></br>
                   <h2>
-                  Call Jeremy today for a free consultation and quote today!
+                  Call Jeremy for a free consultation and quote today!
                   </h2> 
               </p>
           </div>
