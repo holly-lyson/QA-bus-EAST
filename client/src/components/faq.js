d0edb646c5b5c0e14ee558cdd5f1a1660c4c2868
petroortep <78611123+petroortep@users.noreply.github.com>
2021-06-23 20:02:23 -0400

    faq kinda done

--- client/src/components/faq.js
+++ client/src/components/faq.js
@@ -4,39 +4,51 @@ import logo2 from '../surfLogo.png';
 import './header.css';
 // import './menubar';
 import MenuBar from './menubar';
-import './menubar.css';
+import './faq.css';
 
 
 class Faq extends Component {
+  state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
   render() {
     return(
         <div className="blk-background">
         <div className="faq-page">
-          <div className="question-tag">
+        <ul className="questions-list">
+        <li>
+          <div className="question-tag" onClick={this.handleClick}>
              What is the best type of roof?
           </div>
-          <div className="answer-tag">
+          <div className="answer-tag" style={this.state.clicked ? {display:'inline-flex'} : {display:'none'}}>
              Best type of a roof is one you only have to do once!
           </div>
-          <div className="question-tag">
+        </li>
+        
+          <div className="question-tag" onClick={this.handleClick}>
              If my roof leaks do I need a whole new roof?...
           </div>
-          <div className="answer-tag">
+          <div className="answer-tag" style={this.state.clicked ? {display:'inline-flex'} : {display:'none'}}>
              Every case is unique. A leak does not necessarily mean you need a whole new roof!
           </div>
-          <div className="question-tag">
+          <div className="question-tag" onClick={this.handleClick}>
              How long do roofs last?...
           </div>
-          <div className="answer-tag">
+          <div className="answer-tag" style={this.state.clicked ? {display:'inline-flex'} : {display:'none'}}>
              A roof's life is largely dependent on the quality of materials and the trained installers.
           </div>
-          <div className="question-tag">
+          <div className="question-tag" onClick={this.handleClick}>
              How long does it take to replace a roof?...
           </div>
-          <div className="answer-tag">
+          <div className="answer-tag" style={this.state.clicked ? {display:'inline-flex'} : {display:'none'}}>
              Roof are very different. There is no set time length on replacing a roof. 
              Typically one to two days would be average.
           </div>
+        </ul>
+          
         </div>
         </div>
         // <div>
--- client/src/components/referrals.js
+++ client/src/components/referrals.js
@@ -6,30 +6,30 @@ class Referrals extends Component {
   render() {
     return(
         <div className="blk-background">
-        <div className="Ref">
-          <div className="introduction">
-            <p className="intro-title">Refer and Earn</p>
-            <p className="money">$100 cash </p> 
-            <p>per referral</p>
-            <p>as long as the referred customer has a completed service</p>
+          <div className="referrals-container">
+            <div className="introduction">
+              <p className="intro-title">Refer and Earn</p>
+              <p className="money">$100 cash </p> 
+              <p style={{fontSize:'1.8rem'}}>per referral</p>
+              <p>as long as the referred customer has a completed service</p>
+            </div>
+            <div className="ref-form">
+              <div className="form-inputs">
+                <label className="form-label">Your Name</label>
+                <input type="text" name="name" className="form-input" placeholder="Your Name..." />
+              </div>
+              <div className="form-inputs">
+                <label className="form-label">Name of referred person </label>
+                <input type="text" name="name" className="form-input" placeholder="Referred person's name..." />
+              </div>
+              <div className="form-inputs">
+                <label className="form-label">Phone number of referred person</label>
+                <input type="text" name="name" className="form-input" placeholder="Phone number..." />
+              </div>
+              <input type="submit" value="Submit" />
+            </div>
+            
           </div>
-          <div className="form-container">
-            <div className="form-inputs">
-              <label className="form-label">Your Name</label>
-              <input type="text" name="name" className="form-input" placeholder="Your Name..." />
-            </div>
-            <div className="form-inputs">
-              <label className="form-label">Name of referred person </label>
-              <input type="text" name="name" className="form-input" placeholder="Referred person's name..." />
-            </div>
-            <div className="form-inputs">
-              <label className="form-label">Phone number of referred person</label>
-              <input type="text" name="name" className="form-input" placeholder="Phone number..." />
-            </div>
-            <input type="submit" value="Submit" />
-          </div>
-          
-        </div>
         </div>
         // <div>
         //     <h4>Small header</h4>
